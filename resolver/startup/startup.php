6c927d639b0728f85fc81b314310508a77542592
Alexandr Krasnij <php4.drvn@gmail.com>
2019-06-11 11:52:07 +0300

    Fix local

--- resolver/startup/startup.php
+++ resolver/startup/startup.php
@@ -4,39 +4,40 @@ use GraphQL\Utils\BuildSchema;
 
 class ResolverStartupStartup extends Resolver
 {
+    public function index()
+    {
+        if (is_plugin_active('d_vuefront/plugin.php')) {
+            if (! empty($_GET['cors'])) {
+                if (! empty($_SERVER['HTTP_ORIGIN'])) {
+                    header('Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN']);
+                } else {
+                    header('Access-Control-Allow-Origin: *');
+                }
+                header('Access-Control-Allow-Methods: POST, OPTIONS');
+                header('Access-Control-Allow-Credentials: true');
+                header('Access-Control-Allow-Headers: DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Token,token,Cookie,cookie,content-type');
+            }
 
-    public function index() {
-
-        if ( ! empty( $_GET['cors'] ) ) {
-			if ( ! empty( $_SERVER['HTTP_ORIGIN'] ) ) {
-				header( 'Access-Control-Allow-Origin: ' . $_SERVER['HTTP_ORIGIN'] );
-			} else {
-				header( 'Access-Control-Allow-Origin: *' );
-			}
-			header( 'Access-Control-Allow-Methods: POST, OPTIONS' );
-			header( 'Access-Control-Allow-Credentials: true' );
-			header( 'Access-Control-Allow-Headers: DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range,Token,token,Cookie,cookie,content-type' );
-		}
-
-        $this->load->model('startup/startup');
+            $this->load->model('startup/startup');
         
-        try {
-            $resolvers = $this->model_startup_startup->getResolvers();
-            $schema = BuildSchema::build(file_get_contents(DIR_PLUGIN.'schema.graphql'));
-            $rawInput = file_get_contents('php://input');
-            $input = json_decode($rawInput, true);
-            $query = $input['query'];
+            try {
+                $resolvers = $this->model_startup_startup->getResolvers();
+                $schema = BuildSchema::build(file_get_contents(DIR_PLUGIN.'schema.graphql'));
+                $rawInput = file_get_contents('php://input');
+                $input = json_decode($rawInput, true);
+                $query = $input['query'];
 
-            $variableValues = isset($input['variables']) ? $input['variables'] : null;
-            $result = GraphQL::executeQuery($schema, $query, $resolvers, null, $variableValues);
-        } catch (\Exception $e) {
-            $result = [
+                $variableValues = isset($input['variables']) ? $input['variables'] : null;
+                $result = GraphQL::executeQuery($schema, $query, $resolvers, null, $variableValues);
+            } catch (\Exception $e) {
+                $result = [
                 'error' => [
                     'message' => $e->getMessage()
                 ]
             ];
-        }
+            }
     
-        echo json_encode($result);
+            echo json_encode($result);
+        }
     }
 }
--- resolver/startup/wordpress.php
+++ resolver/startup/wordpress.php
@@ -2,6 +2,7 @@
 
 class ResolverStartupWordpress extends Resolver {
 	public function index() {
+        include_once realpath( DIR_PLUGIN . '../../../wp-admin/includes/plugin.php' );
 		require_once realpath( DIR_PLUGIN . '../../../wp-load.php' );
 //        require_once realpath(DIR_PLUGIN.'system/helpers/load.php');
 	}
