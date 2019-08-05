63461f59e95618059495f32da1dee03796d3e95e
Alexandrvn <php4.drvn@gmail.com>
2019-08-05 17:22:21 +0300

    Remove deprecated

--- resolver/startup/wordpress.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class ResolverStartupWordpress extends Resolver {
-	public function index() {
-//        include_once realpath( VF_DIR_PLUGIN . '../../../wp-admin/includes/plugin.php' );
-//		require_once realpath( VF_DIR_PLUGIN . '../../../wp-load.php' );
-//        require_once realpath(VF_DIR_PLUGIN.'system/helpers/load.php');
-	}
-}
